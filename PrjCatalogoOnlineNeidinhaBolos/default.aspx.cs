02c1b15bd72bb4e1fa3ddc2b5c1716bae643b147
unknown <antoniomikael500gmail.com>
2023-05-11 23:02:05 -0300

    corrigido o bug de responsividade

---
+++ PrjCatalogoOnlineNeidinhaBolos/default.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+namespace PrjCatalogoOnlineNeidinhaBolos
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection conexao = new SqlConnection(ConfigurationManager.ConnectionStrings["PrjCatalogoOnlineNeidinhaBolos.Properties.Settings.strC"].ToString());
+
+            var cmd = new SqlCommand();
+            cmd.CommandText = "ps_destaque";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = conexao;
+            conexao.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                string nome = reader.GetString(2);
+                decimal preco = reader.GetDecimal(4);
+                //string descricao = reader.GetString(3);
+                string caminhoImagem = "https://" + reader.GetString(7).Substring(30);
+
+
+
+
+                HtmlGenericControl cardContainer = new HtmlGenericControl("li");
+                cardContainer.Attributes.Add("class", "divCardDestaque");
+                HtmlGenericControl img = new HtmlGenericControl("img");
+                img.Attributes["src"] = caminhoImagem;
+                cardContainer.Controls.Add(img);
+                HtmlGenericControl h3 = new HtmlGenericControl("h2"); // Usa a tag correta para o título
+
+                h3.InnerText = nome;
+                cardContainer.Controls.Add(h3);
+                //HtmlGenericControl p = new HtmlGenericControl("p"); // Usa a tag correta para o texto
+                //p.InnerText = descricao;
+                //cardContainer.Controls.Add(p);
+                HtmlGenericControl span = new HtmlGenericControl("span");
+                span.InnerText = preco.ToString("C");
+                cardContainer.Controls.Add(span);                     // Adiciona o elemento divCard ao container 
+                divDestaqueId.Controls.Add(cardContainer);
+
+            }
+            cmd.Connection.Close();
+        } 
+    }
+}
---
+++ PrjCatalogoOnlineNeidinhaBolos/js/imagenNãoCarregada.js
@@ -0,0 +1,9 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const images = document.querySelectorAll('img');
+    images.forEach(function (image) {
+        image.onerror = function () {
+            this.onerror = null;
+            this.src = '../images/imagempadrao.png';
+        };
+    });
+});
